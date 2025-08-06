1e7ca40cf09166ee54381b1842d045ffab0c75e6
naka-git-hub <naka.rafilipomanana@gmail.com>
2025-08-06 16:29:26 +0200

    Logique orientée objet à propos de la connexion à la base de données

--- config/database.php
+++ config/database.php
@@ -1,2 +1,40 @@
 <?php
 
+//information pour se connecter
+//l'endroit ou est ma database
+$host = "localhost";
+//le nom de la db
+$dbname = "users";
+//identifiant de connexion
+$username = "root";
+//mdp de connexion
+$password = "";
+//port
+$port = 3306;
+//encodage
+$charset = "utf8mb4";
+
+//fonction qui crée et renvoi une connexion à la db
+function dbConnexion(){
+
+    //trandforme mes variable en global (accessible partout)
+    global $host, $dbname, $password, $username, $port, $charset;
+
+    try {
+        //DSN est le nom de la source de données contenant$
+        $dsn = "mysql:host=$host;dbname=$dbname;charset=$charset;port=$port";
+        $pdo = new PDO($dsn, $username, $password);
+         //comment recuperer les exception (erreurs)
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        //comment me renvoyer les données
+        $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+        return $pdo;
+
+
+    } catch (PDOException $e) {
+        die("Erreur durant la co à la db: " . $e->getMessage());
+    }
+
+}
+
